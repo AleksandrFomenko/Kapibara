3a62c04e2e669ccea3e1068f0df26f69b160c501
Aleksandr <umr228@gmail.com>
2023-07-10 18:52:13 +0300

    нропро

--- Kapibara/UISystemName.xaml.cs
+++ Kapibara/UISystemName.xaml.cs
@@ -194,7 +194,6 @@ namespace Kapibara
                     if (elem.LookupParameter(ParameterName) != null && !elem.LookupParameter(ParameterName).IsReadOnly)
                     {
                        cm.setParameterValueByNameToElement(elem,ParameterName,elem.get_Parameter(bp).AsString());
-                       elem.LookupParameter("тест")
                     }
                     
                     foreach (Element subelem in cm.GetSubComponents(elem))
