6cae16fba61090da153918697dcf9c7015768184
AFomenko <umr228@gmail.com>
2023-07-14 16:45:19 +0300

    2

--- Kapibara/WritingToParameterWPF.xaml.cs
+++ Kapibara/WritingToParameterWPF.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Autodesk.Revit.DB;
+using Binding = Autodesk.Revit.DB.Binding;
 
 namespace Kapibara
 {
@@ -25,30 +27,81 @@ namespace Kapibara
             Doc = doc;
         }
         Document Doc;
+        private string parName;
+        private string value;
+        private List<Parameter> allParameters = new List<Parameter>();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            BindingMap bindingMap = Doc.ParameterBindings;
+            DefinitionBindingMapIterator iterator = bindingMap.ForwardIterator();
+            List<String> allParametersStr = new List<String>();
+
+            while (iterator.MoveNext())
+            {
+                Definition definition = iterator.Key as Definition;
+                if (definition != null && definition.ParameterType == ParameterType.Text)
+                {
+                    allParametersStr.Add(definition.Name);
+                }
+            }
 
 
+
+            foreach (String par in allParametersStr)
+            {
+                if (!Parameters.Items.Contains(par))
+                {
+                    Parameters.Items.Add(par);
+                }
+                if (par == "ААА_Раздел")
+                {
+                    Parameters.SelectedItem = "ААА_Раздел";
+                }
+            }
+        }
+        
+    
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            ExecuteTransactionSystemName();
             this.Close();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox textBox = (TextBox)sender;
-            string enteredText = textBox.Text;
+            value = textBox.Text;
 
         }
         private void Parameters_Selection(object sender, SelectionChangedEventArgs e)
         {
+            if (Parameters.SelectedItem != null)
+            {
+                string selectedParameterName = Parameters.SelectedItem.ToString();
+                parName = selectedParameterName;
+            }
 
         }
         public void ExecuteTransactionSystemName()
         {
+            Transaction t = new Transaction(Doc);
+            t.Start("Start");
+            List <Element> elem = new FilteredElementCollector(Doc,Doc.ActiveView.Id).WhereElementIsNotElementType().ToElements().ToList();
+            foreach (Element element in elem)
+            {
+                if (element != null)
+                {
+                    if (element.LookupParameter(parName)!=null && !element.LookupParameter(parName).IsReadOnly)
+                    {
+                        element.LookupParameter(parName).Set(value);
 
+                    }
 
-
-
+                }
+            }
+            t.Commit();
         }
     }
 }
